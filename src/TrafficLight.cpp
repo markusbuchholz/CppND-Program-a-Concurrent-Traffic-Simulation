77544183f7e7e1c45fc981e3c4e3eaa15c13c958
markusbuchholz <markus.x.buchholz@gmail.com>
2019-10-12 15:30:04 +0200

    fix FP.5b

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -39,7 +39,7 @@ void MessageQueue<T>::send(T &&msg)
 
 /* Implementation of class "TrafficLight" */
 
-/* 
+ 
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -50,13 +50,21 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+    while(true){
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        if(_queue_member.receive() == TrafficLightPhase::green)
+        return;
+    }
+
+
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
 {
     return _currentPhase;
 }
-*/
+
 
 void TrafficLight::simulate()
 {
